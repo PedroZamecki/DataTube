f88d9bfbb02622ce1822b09ca804d4645005a9e2
Pedro Zamecki <51971866+PedroZamecki@users.noreply.github.com>
2025-01-12 02:42:28 +0000

    atualização da frase inicial

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 function Home() {
-    return <h1>Primeira implementação da página inicial</h1>
+    return <>
+        <h1>Em homenagem a quem me fez ter essa ideia inicial, meu orgulho diário e meu amor, Naná</h1>
+        <h2> <a href="https://www.youtube.com/@umananaai">Canal do Youtube</a> </h2>
+    </>
 }
 
 export default Home;
