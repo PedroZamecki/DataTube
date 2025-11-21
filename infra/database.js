f1a5360ad7d20a57ac262caefbf2b13ee88d9a34
Pedro Zamecki <pedrozamecki@gmail.com>
2025-11-21 17:21:18 -0300

    fix: handle `client` as `undefined` in `database.js`

--- infra/database.js
+++ infra/database.js
@@ -7,9 +7,10 @@ async function query(queryObject) {
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
-    console.error(`Database error: {}`, error);
+    console.error(`Database error: '${error}'`);
+    throw error;
   } finally {
-    await client.end();
+    await client?.end();
   }
 }
 
