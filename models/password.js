c2f40fabf8f25805e9e20e8833f0563104e448ea
Pedro Zamecki <pedrozamecki@gmail.com>
2025-12-04 18:42:02 -0300

    feat: add pepper for `bryptjs` hashing increased security

--- models/password.js
+++ models/password.js
@@ -2,15 +2,19 @@ import bcryptjs from "bcryptjs";
 
 async function hash(password) {
   const rounds = getNumberOfRounds();
-  return await bcryptjs.hash(password, rounds);
+  return await bcryptjs.hash(passwordWithPepper(password), rounds);
 }
 
 async function compare(password, hash) {
-  return await bcryptjs.compare(password, hash);
+  return await bcryptjs.compare(passwordWithPepper(password), hash);
 }
 
 function getNumberOfRounds() {
   return process.env.NODE_ENV == "production" ? 14 : 1;
+}
+
+function passwordWithPepper(password) {
+  return password + process.env.DATABASE_PEPPER;
 }
 
 const password = { hash, compare };
